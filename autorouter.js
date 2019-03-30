3a45e684d045b073087e94f7c9cc669854a844de
Henry Penton <henry.penton@gmail.com>
2019-03-30 22:57:55 +0000

    Updated to return autorouter as a function

--- autorouter.js
+++ autorouter.js
@@ -1,45 +1,40 @@
-const send = require('koa-send'),
-  fs = require('fs'),
-  render = require('koa-ejs'),
-  path = require('path'),
-  run = require('../../../run.js');
-  
-const publicFolder =  path.join(__dirname, '../../../public');
-const viewsFolder =  path.join(__dirname, '../../../views');
-  
-render(app, {
-  root: viewsFolder,
-  layout: false,
-  viewExt: 'html',
-  cache: false,
-  debug: false
-});
-
-autorouter = async (ctx) => {
-
-  if (!(ctx.path.includes('.'))) {
-    if (ctx.path.length == 1) {
-      await ctx.render('index', await run('index', ctx));
-
-    } else {
-      if (!(pageExists(ctx.path + '.html'))) {
-        await ctx.render('404');
+const send = require("koa-send"),
+  fs = require("fs"),
+  render = require("koa-ejs"),
+  path = require("path"),
+  autorouterSetup = directory => {
+    const autorouter = async ctx => {
+      const run = require(directory + "/run.js");
+      const publicFolder = path.join(directory, "/public");
+      const viewsFolder = path.join(directory, "/views");
+      render(app, {
+        root: viewsFolder,
+        layout: false,
+        viewExt: "html",
+        cache: false,
+        debug: false
+      });
+      if (!ctx.path.includes(".")) {
+        if (ctx.path.length == 1) {
+          await ctx.render("index", await run("index", ctx));
+        } else {
+          if (!pageExists(viewsFolder, ctx.path + ".html")) {
+            await ctx.render("404");
+          } else {
+            await ctx.render(ctx.path.substr(1), await run(ctx.path));
+          }
+        }
       } else {
-        await ctx.render(ctx.path.substr(1), await run(ctx.path));
+        await send(ctx, ctx.path, {
+          root: publicFolder
+        });
       }
-    }
-  } else {
-	  
-    await send(ctx, ctx.path, {
-      root: publicFolder
-    });
-  }
+    };
+    return autorouter;
+  };
 
-}
+module.exports = autorouterSetup;
 
-module.exports = autorouter;
-
-
-pageExists = (page) => {
+pageExists = (viewsFolder, page) => {
   return fs.existsSync(viewsFolder + page);
-}
+};
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Koa = require('koa');
+const Koa = require("koa");
 app = new Koa();
-autorouter = require('./autorouter.js');
+autorouter = require("./autorouter.js");
 
-app.use(autorouter);
-module.exports = function autoroute(){
-	return app;
-}
+module.exports = function autoroute(directory) {
+  app.use(autorouter(directory));
+  return app;
+};
